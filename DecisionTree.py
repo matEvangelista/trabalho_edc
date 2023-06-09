01a9626fb39ea27e777fdfc1cdce978c9ffa0878
Mateus Alcantara <mateus.e.alcantara@outlook.com>
2023-06-08 22:28:14 -0300

    equilíbrio de nash e resultado pareto eficiente para jogadas que dominam outras

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,30 +1,61 @@
 class Tree:
-    value = None
-    left = None  # árvore à esquerda
-    right = None  # árvore à direita
 
     # construtor para nó interno, folha ou raiz
     def __init__(self, value, left_tree=None, right_tree=None):
-        self.value = value
-        self.left = left_tree
-        self.right = right_tree
+        self._value = value
+        self._left = left_tree
+        self._right = right_tree
 
     def __str__(self):
         return self._stringify("")
 
     def _stringify(self, string: str):
-        string += '(' + str(self.value)
-        if self.left is not None:
-            string = self.left._stringify(string)
-        if self.right is not None:
-            string = self.right._stringify(string)
+        string += '(' + str(self._value)
+        if self._left is not None:
+            string = self._left._stringify(string)
+        if self._right is not None:
+            string = self._right._stringify(string)
         return string + ')'
 
-class Output:
-    value: list
+    def nash_equilibrium_output(self):
+        if (self.leftmost_value(0) >= self._right.leftmost_value(0)
+                and (self._left.rightmost_value(0) >= self.rightmost_value(0))):
+            return self._left._best_strategy()
+        return self._right._best_strategy()
 
-    def __init__(self, o1: int, o2: int):
-        self.value = [o1, o2]
+    def pareto_efficient_output(self) -> list:
+        outputs: list = self._get_all_outputs()
+        sum_of_outputs: list = [sum(x) for x in outputs]
+        max_output = max(sum_of_outputs)
+        for output in outputs:
+            if sum(output) == max_output:
+                return output
 
-    def __str__(self):
-        return str(self.value)
+
+    def _get_all_outputs(self) -> list:
+        if self._left is None and self._right is None:
+            return [self._value]
+        outputs: list = []
+        if self._left is not None:
+            outputs += (self._left._get_all_outputs())
+        if self._right is not None:
+            outputs += (self._right._get_all_outputs())
+        return outputs
+
+    def _best_strategy(self) -> list:
+        if self._left._value[1] >= self._right._value[1]:
+            return self._left._value
+        return self._right._value
+
+    def leftmost_value(self, index: int) -> int:
+        if self.is_output():
+            return self._value[index]
+        return self._left.leftmost_value(index)
+
+    def rightmost_value(self, index: int) -> int:
+        if self.is_output():
+            return self._value[index]
+        return self._right.rightmost_value(index)
+
+    def is_output(self) -> bool:
+        return self._left is None and self._right is None
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from DecisionTree import *
+from DecisionTree import Tree
 
-f1 = Tree(Output(-3, -3))
-f2 = Tree(Output(-1, -10))
-f3 = Tree(Output(-10, -1))
-f4 = Tree(Output(-2, -2))
+f1 = Tree([-3, -3])
+f2 = Tree([-1, -10])
+f3 = Tree([-10, -1])
+f4 = Tree([-2, -2])
 
 f5 = Tree("Jogador 2", f1, f2)
 f6 = Tree("Jogador 2", f3, f4)
@@ -11,3 +11,5 @@ f6 = Tree("Jogador 2", f3, f4)
 root = Tree("Jogador 1", f5, f6)
 
 print(root)
+print(root.nash_equilibrium_output())
+print(root.pareto_efficient_output())
